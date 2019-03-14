fcf0bee1dee5b8018d6207fd8721e86265f4468c
Alex Bennett <me@alexben.net>
2019-03-14 15:57:36 -0500

    Added const.py file

---
+++ ges_pkg/ges/core/util/const.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2019 Elexa Consumer Product, Inc.
+#
+# This file is part of the Guardian Device Simulator
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+LADY_NAMES = ['Abigail','Ada','Adele','Adelaide','Adeline','Alice','Alma','Amelia','Ann','Anne','Anna','Annabelle','Audrey','Augusta','Ava','Beatrice','Bernadette','Betsy','Birdie','Blanche','Blythe','Caroline','Celia','Charlotte','Chloe','Clara','Clarice','Clarissa','Colleen','Cora','Clementine','Daisy','Delia','Delilah','Della','Dinah','Dixie','Dora','Dorothy','Dorothea','Edie','Edith','Eilleen','Eleanor','Elise','Eliza','Elizabeth','Ella','Ellie','Eloise','Elsa','Elsie','Emma','Emily','Enid','Esme','Etta','Eudora','Eva','Evie','Eve','Evelyn','Fay','Flora','Frances','Gladys','Gloria','Grace','Georgette','Georgia','Georgina','Greta','Gwendolyn','Harriet','Hattie','Hazel','Helen','Ida','Imogen','Inez','Irene','Iris','Isabella','Isabelle','Isadora','Jane','Jewel','Jocelyn','Julia','June','Justine','Layla','Lenora','Leona','Lila','Liliana','Lillian','Lily','Liza','Lorene','Loretta','Lorraine','Louise','Lucinda','Lucy','Lydia','Madeline','Madelyn','Maggie','Maisie','Mamie','Margaret','Marion','Marilyn','Marjorie','Martha','Matilda','Maureen','Mavis','Maxine','May','Meredith','Millie','Minnie','Miriam','Molly','Muriel','Myra','Nell','Nellie','Neva','Nora','Noreen','Olivia','Opal','Pauline','Pearl','Penelope','Phoebe','Polly','Priscilla','Rosalyn','Rosalind','Rose','Rosemary','Roxanne','Ruby','Ruth','Sadie','Sally','Sarah','Scarlett','Selma','Sophia','Sophie','Stella','Sylvia','Tillie','Trudy','Vera','Victoria','Viola','Violet','Virginia','Vivian','Vivienne','Willa']
